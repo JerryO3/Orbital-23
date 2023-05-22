0e9cf41521b36ce0dcf1078c8a34917c02fe9233
jinyuan0425 <pdmon123@gmail.com>
2023-05-22 19:46:07 +0800

    Update Register.js
    Working registration page.

--- schedule-manager/src/Register.js
+++ schedule-manager/src/Register.js
@@ -1,8 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import './Register.css';
+import { getDatabase, ref, set } from "firebase/database";
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function writeUserData(username, email, password) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + username), {
+    username: username,
+    email: email,
+    password : password
+  });
+}
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
     <div className="container">
       <div className="logo">
@@ -15,15 +32,35 @@ function Login() {
       </h1>
       <div className="loginBox">
         <form className="form">
-          <input type="username" placeholder="Username" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="confirmPassword" placeholder="Confirm Password" />
+          <input 
+            type="username" 
+            placeholder="Username" 
+            name="username" 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} />
+          <input 
+            type="email" 
+            placeholder="Email" 
+            name="email" value={email} 
+            onChange={(e) => setEmail(e.target.value)} />
+          <input 
+            type="password" 
+            placeholder="Password" 
+            name="password" 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} />
+          <input 
+            type="password" 
+            placeholder="Confirm Password" 
+            name="confirmPassword" 
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}/>
           <button type="submit">
             <Link to="/landing">Already Have an Account?</Link>
           </button>
           <button type="registrationButton">
-            <Link to="/register">Submit</Link>
+            {writeUserData(username, email, password)}
+            <Link to="/submit">Submit</Link>
           </button>
         </form>
       </div>
