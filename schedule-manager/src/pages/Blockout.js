9b78bb114c41047654d901229a2a2aa102bafd1b
jinyuan0425 <pdmon123@gmail.com>
2023-06-06 14:56:12 +0800

    Blockout function + page

--- schedule-manager/src/pages/Blockout.js
+++ schedule-manager/src/pages/Blockout.js
@@ -1,10 +1,76 @@
 import React from "react";
+import { useState } from 'react';
+import logo from '../assets/logo.png';
+import * as fn from "../backend/functions";
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 
 function Blockout() { 
+  const [name, setName] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [endTime, setEndTime] = useState("");
+  // const [hasAttempted, setHasAttempted] = useState(false);
+
   return (
-    <h1>
-      Welcome to the Blockout Page!
-    </h1>
+    <div className="container">
+      <div className="logo">
+          <img src={logo} alt="Schedule Manager" />
+      </div>
+      <h1 className="welcomeMessage">
+        Set a Blockout Period.
+      </h1>
+      <div className="loginBox">
+        <form className="form" onSubmit={(e) => e.preventDefault()}>
+
+          <input
+            type="name"
+            placeholder="Blockout Name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)} />
+
+          <input
+            type="date"
+            placeholder="Start Date"
+            name="date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)} />
+
+          <input
+            type="time"
+            placeholder="Start Time"
+            name="time"
+            value={startTime}
+            onChange={(e) => setStartTime(e.target.value)} />
+
+          <input
+            type="date"
+            placeholder="End Date"
+            name="date"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)} />
+
+          <input
+            type="time"
+            placeholder="End Time"
+            name="time"
+            value={endTime}
+            onChange={(e) => setEndTime(e.target.value)} />
+
+          <button
+              type="submit"
+              onClick={
+                () => {
+                        fn.newBlockoutByStartEnd(name, startDate, startTime, endDate, endTime);
+                      }
+                }
+            >
+            Create
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
